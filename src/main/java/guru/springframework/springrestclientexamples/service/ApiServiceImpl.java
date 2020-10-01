52ca7591f8e2e5ce3efbcce0d5876f1c20f2beaf
Sarang <sarangdp@gmail.com>
2020-10-01 08:15:19 -0400

    Adding controller

--- src/main/java/guru/springframework/springrestclientexamples/service/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/service/ApiServiceImpl.java
@@ -2,8 +2,10 @@ package guru.springframework.springrestclientexamples.service;
 
 import guru.springframework.api.domain.User;
 import guru.springframework.api.domain.UserData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -11,15 +13,22 @@ import java.util.List;
 public class ApiServiceImpl implements ApiService {
 
     RestTemplate restTemplate;
+    String api_url;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url) {
         this.restTemplate = restTemplate;
+        this.api_url = api_url;
     }
 
     @Override
     public List<User> getUsersList(Integer limit) {
 
-        UserData userData = restTemplate.getForObject("http://apifaketory.com/api/user?limit=" + limit, UserData.class);
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .fromUriString(api_url)
+                .queryParam("limit", limit);
+
+        UserData userData = restTemplate.getForObject(builder.toUriString(), UserData.class);
+
         return userData.getData();
     }
 }
